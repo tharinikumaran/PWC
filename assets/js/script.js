6d472d2c55264360323dd31a028ac3668d9390db
tharini sivakumaran <tharinidivya@hotmail.com>
2017-03-10 11:47:04 +1100

    Upload new source code
    Upload new source code

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	$('#fullname').on('input', function() {
+	    this.value = this.value.replace(/[^a-zA-Z\s]/g, ''); 
+
+	  });
+	$('.button').click(function(){
+		
+		/* button code goes here */
+		
+	});
+
+	$('.submit').click(function(){
+
+		/* submit code goes here */
+		
+	});
+	
+});
